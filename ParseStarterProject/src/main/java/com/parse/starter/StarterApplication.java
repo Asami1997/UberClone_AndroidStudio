f18daa876920e6349465d9e8e89be943e485ec68
Ahmad <asami1997@gmail.com>
2017-09-16 20:54:07 +0800

    Update StarterApplication.java

--- ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
+++ ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -29,23 +29,7 @@ public class StarterApplication extends Application {
             .build()
     );
 
-    /*
-    ParseObject object = new ParseObject("ExampleObject");
-    object.put("myNumber", "123");
-    object.put("myString", "rob");
-
-    object.saveInBackground(new SaveCallback () {
-      @Override
-      public void done(ParseException ex) {
-        if (ex == null) {
-          Log.i("Parse Result", "Successful!");
-        } else {
-          Log.i("Parse Result", "Failed" + ex.toString());
-        }
-      }
-    });
-    */
-
+ 
     //ParseUser.enableAutomaticUser();
 
     ParseACL defaultACL = new ParseACL();
